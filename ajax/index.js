97ad7d685e9c3e2a4d4ff6735171feaff4505508
kikon123 <tatquan2001@gmail.com>
2024-05-29 10:20:27 +0700

    up update

--- ajax/index.js
+++ ajax/index.js
@@ -29,16 +29,14 @@ function addNewSmartPhone() {
 function successHandler() {
     $.ajax({
         type: "GET",
-        //tên API
         url: "http://localhost:8080/api/smartphones",
-        //xử lý khi thành công
         success: function (data) {
-            // hiển thị danh sách ở đây
             let content = '<table id="display-list" border="1"><tr>\n' +
                 '    <th>Nhà sản xuất</th>\n' +
                 '    <th>Mẫu</th>\n' +
                 '    <th>Giá</th>\n' +
-                '    <th>Xóa</th>\n' +
+                '    <th class="btn">Sửa</th>\n' +
+                '    <th class="btn">Xóa</th>\n' +
                 '</tr>';
             for (let i = 0; i < data.length; i++) {
                 content += getSmartphone(data[i]);
@@ -47,6 +45,7 @@ function successHandler() {
             document.getElementById('smartphoneList').innerHTML = content;
             document.getElementById('smartphoneList').style.display = "block";
             document.getElementById('add-smartphone').style.display = "none";
+            document.getElementById('update-smartphone').style.display = "none";
             document.getElementById('display-create').style.display = "block";
             document.getElementById('title').style.display = "block";
         }
@@ -73,4 +72,59 @@ function deleteSmartphone(id) {
         //xử lý khi thành công
         success: successHandler
     });
+}
+let currentUpdateId = null;
+
+function showUpdateForm(smartphone) {
+    $('#update-producer').val(smartphone.producer);
+    $('#update-model').val(smartphone.model);
+    $('#update-price').val(smartphone.price);
+    currentUpdateId = smartphone.id;
+
+    document.getElementById('smartphoneList').style.display = "none";
+    document.getElementById('add-smartphone').style.display = "none";
+    document.getElementById('update-smartphone').style.display = "block";
+    document.getElementById('display-create').style.display = "none";
+    document.getElementById('title').style.display = "none";
+}
+
+function updateSmartPhone() {
+    let producer = $('#update-producer').val();
+    let model = $('#update-model').val();
+    let price = $('#update-price').val();
+    let updatedSmartphone = {
+        producer: producer,
+        model: model,
+        price: price
+    };
+
+    $.ajax({
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        type: "PUT",
+        data: JSON.stringify(updatedSmartphone),
+        url: `http://localhost:8080/api/smartphones/${currentUpdateId}`,
+        success: successHandler
+    });
+    event.preventDefault();
+}
+
+function getSmartphone(smartphone) {
+    return `<tr>
+        <td>${smartphone.producer}</td>
+        <td>${smartphone.model}</td>
+        <td>${smartphone.price}</td>
+        <td class="btn"><button class="editSmartphone" onclick='showUpdateForm(${JSON.stringify(smartphone)})'>Sửa</button></td>
+        <td class="btn"><button class="deleteSmartphone" onclick="deleteSmartphone(${smartphone.id})">Xóa</button></td>
+    </tr>`;
+}
+
+function displayFormCreate() {
+    document.getElementById('smartphoneList').style.display = "none";
+    document.getElementById('add-smartphone').style.display = "block";
+    document.getElementById('update-smartphone').style.display = "none";
+    document.getElementById('display-create').style.display = "none";
+    document.getElementById('title').style.display = "none";
 }
